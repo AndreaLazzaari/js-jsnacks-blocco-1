6c23f0419d132fc8c751e6b12cb52383bd5f1654
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-10 12:12:44 +0100

    esercizio corretto dopo correzzione Riccardo

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,12 +1,13 @@
 
 // chiedere all'utente con due prompt gli anni di due persone e comunicare quale delle due è più grande
 
-const anniPrimaPersona = prompt('Inserisci gli anni della Prima persona')
-const anniSecondaPersona = prompt('Inserisci gli anni della Seconda persona')
+const anniPrimaPersona = parseInt(prompt('Inserisci gli anni della Prima persona'));
+const anniSecondaPersona = parseInt(prompt('Inserisci gli anni della Seconda persona'));
 
 if (anniPrimaPersona > anniSecondaPersona) {
-    prompt = ('anniPrimaPersona')
-} else (anniPrimaPersona < anniSecondaPersona) {
-    prompt = ('anniSecondaPersona')
-
+    console.log('Anni prima persona sono maggiori');
+} else if (anniPrimaPersona < anniSecondaPersona) {
+    console.log('Anni seconda persona sono maggiori');
+} else {
+    console.log('Le persone hanno la stessa età');
 }
