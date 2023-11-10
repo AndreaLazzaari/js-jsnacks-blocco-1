5246cf7949a7cfc03b7b99773b2871077320f304
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-10 11:52:40 +0100

    inizio snack-1

---
+++ snack-1/js/script.js
@@ -0,0 +1,7 @@
+
+// chiedere all'utente con due prompt gli anni di due persone e comunicare quale delle due è più grande
+
+const anniPrimaPersona = parseInt('Inserisci gli anni della Prima persona')
+const anniSecondaPersona = ('Inserisci gli anni della Seconda persona')
+
+if (anniPrimaPersona > anniSecondaPersona)
