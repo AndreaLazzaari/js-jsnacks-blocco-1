bfb19fa35eb16d5b7ce7f6af95112145e6ee50d7
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-16 17:28:44 +0100

    prova snack-8 utilizzando while + snack-7 inserito solo prompt

--- snack-7/script.js
+++ snack-7/script.js
@@ -1,6 +1,5 @@
 // Chiedi un numero di 7 cifre all'utente e calcola la somma di tutte le cifre che compongono il numero.
 
 let numeroUtente = parseInt(prompt('inserisci un numero'));
-numeroUtente.toPrecision(7);
 
 console.log(numeroUtente)
--- snack-8/script.js
+++ snack-8/script.js
@@ -1,2 +1,12 @@
 // Chiedi all'utente due numeri, uno piu' piccolo (due cifre) e uno piu' grande (almeno quattro cifre):
 // moltiplica il primo numero per 2 finche' non arriva ad essere maggiore del secondo numero
+
+let primoNumero = parseInt(prompt('Inserisci un numero'));
+let secondoNumero = parseInt(prompt('Inserisci un numero'));
+
+console.log(Math.max(primoNumero, secondoNumero));
+
+while (primoNumero < secondoNumero) {
+    primoNumero * 2
+    
+}
