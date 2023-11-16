771f0588c5848298013bdb91f868de3451375297
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-16 16:54:10 +0100

    cambiate parentesi a listaNumeri.push e esercizio completato e funzionante

--- snack-6/script.js
+++ snack-6/script.js
@@ -10,7 +10,7 @@ for (let i = 0; i < 6; i++ ){
      console.log(numeroUtente)
 
 if (numeroUtente % 2 == 1){
-       listaNumeriDispari.push[numeroUtente]
+       listaNumeriDispari.push(numeroUtente)
     } 
     
 }
@@ -22,17 +22,4 @@ console.log(listaNumeriDispari.length)
 
 
 
-// var numero;
-// var contap = 0, pari
-// contad = 0; dispari
 
-// for (var i = 0; i < 20; i++){
-//   numero = parseInt(prompt('Inserisci un numero'));
-//   if (numero % 2 == 0) {
-//     contap++;
-//   } else {
-//     contad++;
-//   }
-// }
-
-// alert(contap, contad);
